5ba2dc606b8bc03608e59c309ab4ef2e8dd907e7
Julien Caucheteux <jcauchet@c2r11p3.42nice.fr>
2022-10-02 21:41:50 +0200

    CPP01 ex04 done

--- module-01/ex04/main.cpp
+++ module-01/ex04/main.cpp
@@ -6,40 +6,42 @@
 /*   By: jcauchet <jcauchet@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/02 15:20:28 by jcauchet          #+#    #+#             */
-/*   Updated: 2022/10/02 18:05:00 by jcauchet         ###   ########.fr       */
+/*   Updated: 2022/10/02 21:41:02 by jcauchet         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <iostream>
 #include <string>
 #include <fstream>
+#include <sys/stat.h>
 
 int main(int ac, char **av)
 {
 	if (ac != 4)
 	{
 		std::cout << "Error: incorrect syntax." << std::endl;
-		return (1);
+		return 1;
 	}
-	std::ifstream input;
-	input.seekg(0, std::ios::end);
-	// if (!input.good())
-	// {
-	// 	std::cout << "Error: path is a directory." << std::endl;
-	// 	return (1);
-	// }
-	input.open(av[1], std::ios::in);
+	struct stat path_stat;
+    stat(av[1], &path_stat);
+    int is_dir =  S_ISDIR(path_stat.st_mode);
+	if (is_dir)
+	{
+		std::cout << "Error: path is a directory." << std::endl;
+		return 1;
+	}
+	std::string	input_name(av[1]);
+	std::ifstream input(input_name);
 	if (!input)
 	{
 		std::cout << "Error: wrong filename." << std::endl;
-		return (1);
+		return 1;
 	}
-	std::ofstream output;
-	std::string output_name = std::string(av[1]) + ".replace";
-	output.open(output_name, std::ios::out);
-	std::string line;
+	std::string output_name = input_name + ".replace";
+	std::ofstream output(output_name);
 	while (input)
 	{
+		std::string line;
 		getline(input, line);
 		if (!line[0])
 			break;
@@ -53,6 +55,4 @@ int main(int ac, char **av)
 		}
 		output << line << "\n";
 	}
-	input.close();
-	output.close();
 }
