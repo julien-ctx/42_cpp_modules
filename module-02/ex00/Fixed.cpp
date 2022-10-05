d53e7a90af8a33bbe324cc08a0e67d78b59cb315
Julien Caucheteux <jcauchet@c1r5p3.42nice.fr>
2022-10-05 12:15:13 +0200

    ex00 module02 done

--- module-02/ex00/Fixed.cpp
+++ module-02/ex00/Fixed.cpp
@@ -12,29 +12,37 @@
 
 #include "Fixed.hpp"
 
-Fixed::Fixed() : _i(0) {}
+Fixed::Fixed() : _i(0)
+{
+	std::cout << "Default constructor called" << std::endl;
+}
 
 Fixed::Fixed(Fixed const &src)
 {
+	std::cout << "Copy constructor called" << std::endl;
 	*this = src;
 }
 
 int	Fixed::getRawBits() const
 {
+	std::cout << "getRawBits member function called" << std::endl;
 	return this->_i;
 }
 
 void Fixed::setRawBits(int const raw)
 {
 	this->_i = raw;
-
 }
 
 Fixed &Fixed::operator=(Fixed const &rhs)
 {
+	std::cout << "Copy assignment operator called" << std::endl;
 	if (this != &rhs)
 		this->_i = rhs.getRawBits();
 	return *this;
 }
 
-Fixed::~Fixed () {}
+Fixed::~Fixed ()
+{
+	std::cout << "Destructor called" << std::endl;
+}
--- module-02/ex00/main.cpp
+++ module-02/ex00/main.cpp
@@ -6,12 +6,33 @@
 /*   By: jcauchet <jcauchet@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/05 11:13:53 by jcauchet          #+#    #+#             */
-/*   Updated: 2022/10/05 11:35:50 by jcauchet         ###   ########.fr       */
+/*   Updated: 2022/10/05 12:14:56 by jcauchet         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Fixed.hpp"
 
-int main()
+int main(void)
 {
+	{
+		std::cout << "---------42 Main----------" << std::endl;
+		Fixed a;
+		Fixed b(a);
+		Fixed c;
+		c = b;
+		std::cout << a.getRawBits() << std::endl;
+		std::cout << b.getRawBits() << std::endl;
+		std::cout << c.getRawBits() << std::endl;
+	}
+	// {
+	// 	std::cout << std::endl << "---------My Main----------" << std::endl;
+	// 	Fixed a;
+	// 	a.setRawBits(42);
+	// 	std::cout << a.getRawBits() << std::endl;
+	// 	Fixed b(a);
+	// 	std::cout << b.getRawBits() << std::endl;
+	// 	Fixed c;
+	// 	c = b;
+	// 	std::cout << c.getRawBits() << std::endl;
+	// }
 }
