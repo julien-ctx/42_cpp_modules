a69826a22fbf1d6d8b4dad649aaf8fb819e9d101
Julien Caucheteux <jcauchet@c2r9p3.42nice.fr>
2022-10-06 18:11:50 +0200

    Module 02 finished

--- module-02/ex03/Point.cpp
+++ module-02/ex03/Point.cpp
@@ -6,7 +6,7 @@
 /*   By: jcauchet <jcauchet@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/06 12:18:20 by jcauchet          #+#    #+#             */
-/*   Updated: 2022/10/06 17:03:41 by jcauchet         ###   ########.fr       */
+/*   Updated: 2022/10/06 17:48:18y jcauchet         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,7 +18,10 @@ Point::Point() : _x(0), _y(0) {}
 Point &Point::operator=(Point const &rhs)
 {
 	if (this != &rhs)
-		Fixed(const_cast<Fixed *>(this->_x)) = Fixed(const_cast<Fixed *>(rhs._x));
+	{
+		const_cast<Fixed &>(this->_x) = rhs._x;
+		const_cast<Fixed &>(this->_y) = rhs._y;
+	}
 	return *this;
 }
 
--- module-02/ex03/bsp.cpp
+++ module-02/ex03/bsp.cpp
@@ -6,25 +6,27 @@
 /*   By: jcauchet <jcauchet@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/06 14:05:05 by jcauchet          #+#    #+#             */
-/*   Updated: 2022/10/06 16:54:33 by jcauchet         ###   ########.fr       */
+/*   Updated: 2022/10/06 18:10:32 by jcauchet         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Point.hpp"
 #include "Fixed.hpp"
 
-Fixed area(Point const a, Point const b, Point const c)
+float area(Point const a, Point const b, Point const c)
 {
-	Fixed ret(float(a.getX() * (b.getY() - c.getY()) + b.getX() * (c.getY() - a.getY() + c.getX() * (a.getY() - b.getY()/2.0))));
+	float ret((a.getX() * (b.getY() - c.getY()) + b.getX() * (c.getY() - a.getY()) + c.getX() * (a.getY() - b.getY())) / 2.0);
+	if (ret < 0)
+		ret = -ret;
 	return ret;
 }
 
 bool bsp(Point const a, Point const b, Point const c, Point const point)
 {
-	Fixed A = area(a, b, c);
-	Fixed A1 = area(point, b, c);
-	Fixed A2 = area(a, point, c);
-	Fixed A3 = area(a, b, point);
+	float A = area(a, b, c);
+	float A1 = area(point, b, c);
+	float A2 = area(a, point, c);
+	float A3 = area(a, b, point);
 	
-	return (A.toInt() == A1.toInt() + A2.toInt() + A3.toInt());
+	return (A == A1 + A2 + A3);
 }
