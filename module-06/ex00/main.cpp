8724a60c3e792cfc224903653519be953a8bb298
julien-ctx <jdaodkql@gmail.com>
2022-10-16 11:48:31 +0200

    Improved numbers convertor a lot

--- module-06/ex00/main.cpp
+++ module-06/ex00/main.cpp
@@ -5,35 +5,69 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: jcauchet <jcauchet@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2022/10/15 16:32:20 by jcauchet          #+#    #+#             */
-/*   Updated: 2022/10/15 17:17:26 by jcauchet         ###   ########.fr       */
+/*   Created: 2022/10/16 10:38:36 by jcauchet          #+#    #+#             */
+/*   Updated: 2022/10/16 11:47:49 by jcauchet         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <iostream>
 #include <string>
+#include <iostream>
 
-void	char_handling(char *s)
+#define ASCII_TYPE 42
+#define FLOAT_TYPE 43
+#define DOUBLE_TYPE 44
+#define SPECIAL_CASES 45
+
+void	char_handling(std::string str, int type)
 {
-	int	nb = atoi(s);
-	if (nb < 32 || nb > 127)
-		std::cout << "Non displayable" << std::endl;
-	else
-		std::cout << '\'' <<static_cast<char>(nb) << '\'' << std::endl;
+	
 }
 
-void	int_handling(char *s)
+void	int_handling(std::string str, int type)
+{
+	
+}
+
+void	float_handling(std::string str, int type)
 {
 
 }
 
-void	float_handling(char *s)
+void	double_handling(std::string str, int type)
 {
-
+	
 }
 
-void	double_handling(char *s)
+int	check_type(std::string str)
 {
+	std::string special_cases[7] = {"+inf", "-inf", "inf", "+inff", "-inff", "inff", "nan"};
+
+	for (int i = 0; i < 7; i++)
+		if (str == special_cases[i])
+			return SPECIAL_CASES;
+	int f = 0; int d = 0; int b = 0; int i = -1;
+	if (str[0] == '+' || str[0] == '-')
+		i++;
+	while (str[++i])
+	{
+		if (str[i] == '.')
+			d++;
+		else if (str[i] == 'f')
+			f++;
+		else if (str[i] < 48 || str[i] > 57)
+			b++;
+	}
+	if (str.length() > 1)
+	{
+		if (b || f > 1 || d > 1)
+			throw std::exception();
+		else if (d && f)
+			return FLOAT_TYPE;
+		else if (d)
+			return DOUBLE_TYPE;	
+		std::stoi(str);
+	}
+	return ASCII_TYPE;	
 	
 }
 
@@ -41,11 +75,23 @@ int main(int ac, char **av)
 {
 	if (ac != 2)
 	{
-		std::cout << "Please use a correct format: ./convert value\n";
+		std::cout << "Please use a correct command format: ./convert [Number]\n";
 		return 1;
 	}
-	char_handling(av[1]);
-	int_handling(av[1]);
-	float_handling(av[1]);
-	double_handling(av[1]);
+	std::string str = static_cast<std::string>(av[1]);
+	int	type;
+	try
+	{
+		type = check_type(str);
+	}
+	catch (std::exception &e)
+	{
+		std::cout << "Please use a correct number format\n";
+		return 1;
+	}
+	char_handling(str, type);
+	int_handling(str, type);
+	float_handling(str, type);
+	double_handling(str, type);
+	return 0;
 }
