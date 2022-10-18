dcf4bd3c08299012d4c36d32bf6cc491f1ae9369
julien-ctx <jdaodkql@gmail.com>
2022-10-18 19:37:40 +0200

    Started stack exercise

---
+++ module-08/ex02/MutantStack.hpp
@@ -0,0 +1,23 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   MutantStack.hpp                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jcauchet <jcauchet@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/10/18 19:33:21 by jcauchet          #+#    #+#             */
+/*   Updated: 2022/10/18 19:33:58 by jcauchet         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#pragma once
+
+#include <stack>
+#include <iostream>
+#include <algorithm>
+#include <vector>
+
+class MutantStack
+{
+
+};
