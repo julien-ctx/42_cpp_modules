0dda066a48afc3b8959f4c4f6a16d60035383c37
julien-ctx <jdaodkql@gmail.com>
2022-10-18 14:15:38 +0200

    Ex00 module 08 done

--- module-08/ex00/easyfind.hpp
+++ module-08/ex00/easyfind.hpp
@@ -6,23 +6,23 @@
 /*   By: jcauchet <jcauchet@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/18 10:42:39 by jcauchet          #+#    #+#             */
-/*   Updated: 2022/10/18 12:09:40 by jcauchet         ###   ########.fr       */
+/*   Updated: 2022/10/18 13:47:35 by jcauchet         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #pragma once
 
 #include <iostream>
-#include <string>
 #include <algorithm>
-#include <array>
 #include <vector>
+#include <list>
+#include <deque>
 
 template<typename T>
 bool	easyfind(T array, int nb)
 {
-	for (std::vector<int>::iterator it = array.begin(); it != array.end(); ++it )
-		if (*it == nb)
-			return true;
-	return false;
+	typename T::iterator it = find(array.begin(), array.end(), nb);
+	if (it == array.end())
+		return false;
+	return true;
 }
--- module-08/ex00/main.cpp
+++ module-08/ex00/main.cpp
@@ -6,7 +6,7 @@
 /*   By: jcauchet <jcauchet@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/18 10:48:03 by jcauchet          #+#    #+#             */
-/*   Updated: 2022/10/18 11:56:17 by jcauchet         ###   ########.fr       */
+/*   Updated: 2022/10/18 13:51:48 by jcauchet         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,8 +14,31 @@
 
 int main()
 {
-	// Testing with vectors
 	int values[5] = {42, 43, 44, 45, 46};
+
+	// Testing with vectors
+	std::cout << "Vectors:\n";
 	std::vector<int> vect(values, values + sizeof(values) / sizeof(int));
-	easyfind(vect, 41) ? std::cout << "Yes\n" : std::cout << "No\n";
+	easyfind(vect, 42) ? std::cout << "Yes\n" : std::cout << "No\n";
+	easyfind(vect, -10) ? std::cout << "Yes\n" : std::cout << "No\n";
+	easyfind(vect, 46) ? std::cout << "Yes\n" : std::cout << "No\n";
+
+	
+	std::cout << "\n\nLists:\n";
+	std::list<int> list(values, values + sizeof(values) / sizeof(int));
+	easyfind(list, 42) ? std::cout << "Yes\n" : std::cout << "No\n";
+	easyfind(list, -10) ? std::cout << "Yes\n" : std::cout << "No\n";
+	easyfind(list, 46) ? std::cout << "Yes\n" : std::cout << "No\n";
+	std::cout << "Last element of the list is removed:\n";
+	list.pop_back();
+	easyfind(list, 46) ? std::cout << "Yes\n" : std::cout << "No\n";
+	list.push_back(46);
+	std::cout << "Then it comes back:\n";
+	easyfind(list, 46) ? std::cout << "Yes\n" : std::cout << "No\n";
+
+	std::cout << "\n\nDeque:\n";
+	std::deque<int> deque(values, values + sizeof(values) / sizeof(int));
+	easyfind(deque, 44) ? std::cout << "Yes\n" : std::cout << "No\n";
+	easyfind(deque, 43243) ? std::cout << "Yes\n" : std::cout << "No\n";
+	easyfind(deque, 41) ? std::cout << "Yes\n" : std::cout << "No\n";
 }
